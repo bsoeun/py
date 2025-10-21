06bfe152e0fbe2b9d31e2b07b58438374e9eb1b5
soeun <2501078@student.hywoman.ac.kr>
2025-10-21 19:41:02 +0900

    세번쩨과제1커밋

---
+++ 세번쩨과제.py
@@ -0,0 +1,55 @@
+#과제11
+# a = list(map(int, input("숫자 5개 입력:").split()))
+# b = map(int, input("숫자 5개 입력:").split())
+# a.extend(b)
+# print(a)
+
+# a = list(map(int, input("숫자 5개 입력:").split()))
+# b = map(int, input("숫자 5개 입력:").split())
+# a.append(b)
+# print(a)          #오류 map이 있어서 인 것 같음 왜 그런건지?
+
+#과제12
+# a = list(map(int, input("숫자 5개 입력:").split()))
+# a.pop()
+# a.pop()
+# print(a)
+
+#과제13
+# a = list(map(int, input("숫자 5개 입력:").split()))
+
+# for index, value in enumerate(a):
+#     # print(101. value,)????
+    
+#과제 14
+# a =[10, 20, 30, 40, 30, 20, 10]
+# print(a.count(20))
+
+#과제15
+# a = list(map(int, input("숫자 10개 입력:").split()))
+# smallest =a[0]
+# largest =a[0]
+
+# for i in a:
+#     if i < smallest:
+#         smallest = i
+# for i in a:
+#     if i > largest:
+#         largest = i
+
+# print(smallest,largest)
+
+# a = list(map(int, input("숫자 10개 입력:").split()))
+# b= sum(a)-min(a)-max(a)
+# print(b)
+
+#과제17
+# a = [10, 20, 30, 40, 30, 20, 10]
+# b = a.count(20)
+# for i in range(b):
+#     a.remove(20)
+# print(a)
+
+#과제18
+a = 
+
