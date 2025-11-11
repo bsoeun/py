a486a9eb5cfc42e07477662208cc63b1c3107e41
soeun <2501078@student.hywoman.ac.kr>
2025-11-11 19:42:55 +0900

    네번째과제1커밋

--- 과제 #25~#31.py
+++ 과제 #25~#31.py
@@ -16,3 +16,31 @@
 # park = {'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83}
 # parkkeys = dict.fromkeys(park, 100)
 # print(parkkeys)
+
+# 28
+# lee = {'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83}
+# print('english'in lee)
+# if 'english' in lee:
+#     del lee['english']
+# print(lee)
+
+# 29
+# lim = dict({'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83})
+# print(lim.keys(), lim.values())
+# print(lim.items())
+
+# 30
+# for i in lim.values():
+#     if i >= 90:
+#         print(i)
+
+# choi = { 'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83 }
+# scores = {subject: score for subject, score in choi.items() if score >= 90}
+# print(scores.keys())
+
+# 31
+
+yoo = { 'korean': 94, 'english': 91, 'mathematics': 89, 'science': 83 }
+
+average = sum(yoo.values()) / len(yoo)
+print("평균 점수:", average)
